0e34a0189e36b766f42b7fff4b2d94eade850fc8
EmilsoSilva <EmilsoSilva@LAPTOP-8BNO6U21.nox5.lan>
2018-09-06 20:41:35 -0300

    Código Criança Esperança switch case

---
+++ src/criancaesperanca/CriancaEsperanca.java
@@ -0,0 +1,40 @@
+
+package criancaesperanca;
+
+import java.util.Scanner;
+
+public class CriancaEsperanca {
+
+    public static void main(String[] args) {
+       int doacao, valor;
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Digite a doação desejada:");
+        doacao = leia.nextInt();
+        switch (doacao){
+            case 10:
+                System.out.println("Você doou 10 Reais!Obrigado!");
+                 break;
+            case 15:
+                System.out.println("Você doou 15 Reais!obrigado!");
+                 break;
+            case 20:
+                System.out.println("Você doou 20 Reais!obrigado!");
+                 break;
+            case 40:
+                System.out.println("Você doou 40 Reais!Obrigado!");
+                 break;
+            case 5:
+                System.out.println("Qual valor você deseja doar!");
+                valor = leia.nextInt();
+                                System.out.println("Sua doação foi de:"+valor);
+
+                break;
+            default:
+                System.out.println("para cancelar a doacao!");
+                
+          
+        }
+                
+    }
+    
+}
